f26bc6371220f2acabf0e739ac5414463471370b
Ravi Parmar <sit451@sit.local>
2024-02-12 18:27:55 +0530

    small changes in sidebar

--- Asest/js/index.js
+++ Asest/js/index.js
@@ -1,38 +1,52 @@
 function expand() {
     let b = document.getElementById("slidbar");
     let c=document.getElementById("content");
-    try{
-        b.attributes.removeNamedItem("style");
-        c.attributes.removeNamedItem("style");
+    if(b.classList.contains("slidbar-toggle")){
+        b.classList.replace("slidbar-toggle","slidbar-toggled");
+    }else{
+        
+        b.classList.replace("slidbar-toggled","slidbar-toggle");
+        
     }
-    catch{}
-    if(window.screen.width>=992){
-        if(b.classList.contains("col-lg-1")){
-            b.classList.replace("col-lg-1","col-lg-2");
-            c.classList.replace("col-lg-11","col-lg-10")
-        }
-        else{
-            b.classList.replace("col-lg-2","col-lg-1");
-            c.classList.replace("col-lg-10","col-lg-11");
-        }
-    }
-     else if (window.screen.width >= 767) {
-        if(b.classList.contains("col-md-1")){
-            b.classList.replace("col-md-1","col-md-2");
-            c.classList.replace("col-md-11","col-md-10")
-        }
-        else{
-            b.classList.replace("col-md-2","col-md-1");
-            c.classList.replace("col-md-10","col-md-11");
-        }
-    }
-    else {
-        if (b.classList.contains("d-none")) {
-            b.style.width = "20%";
-            b.classList.remove('d-none');
-        }
-        else {
-            b.classList.add('d-none');
-        }
-    }
+
+    // let c=document.getElementById("content");
+    // try{
+    //     b.attributes.removeNamedItem("style");
+    //     c.attributes.removeNamedItem("style");
+    // }
+    // catch{}
+    // if(window.screen.width>=992){
+    //     if(b.classList.contains("col-lg-1")){
+    //         b.classList.replace("col-lg-1","col-lg-2");
+    //         c.classList.replace("col-lg-11","col-lg-10");
+    //         document.getElementsByClassName("log-name")[0].style.display="inline";
+    //         document.getElementsByClassName("sidebar-item-name")[0].style.display="inline";
+    //     }
+    //     else{
+
+    //         b.classList.replace("col-lg-2","col-lg-1");
+    //         c.classList.replace("col-lg-10","col-lg-11");
+    //         document.getElementsByClassName("log-name")[0].style.display="none";
+    //         document.getElementsByClassName("sidebar-item-name")[0].style.display="none";
+    //     }
+    // }
+    //  else if (window.screen.width >= 767) {
+    //     if(b.classList.contains("col-md-1")){
+    //         b.classList.replace("col-md-1","col-md-2");
+    //         c.classList.replace("col-md-11","col-md-10")
+    //     }
+    //     else{
+    //         b.classList.replace("col-md-2","col-md-1");
+    //         c.classList.replace("col-md-10","col-md-11");
+    //     }
+    // }
+    // else {
+    //     if (b.classList.contains("d-none")) {
+    //         b.style.width = "20%";
+    //         b.classList.remove('d-none');
+    //     }
+    //     else {
+    //         b.classList.add('d-none');
+    //     }
+    // }
 }
