22271519204f2ee21e581189b2b7d6cdbe9d9002
raviparmar9983 <parmarravi1162@gmail.com>
2024-02-13 18:43:43 +0530

    sidebar changes

--- Asest/js/index.js
+++ Asest/js/index.js
@@ -1,52 +1,25 @@
-function expand() {
-    let b = document.getElementById("slidbar");
-    let c=document.getElementById("content");
-    if(b.classList.contains("slidbar-toggle")){
-        b.classList.replace("slidbar-toggle","slidbar-toggled");
-    }else{
-        
-        b.classList.replace("slidbar-toggled","slidbar-toggle");
-        
+function expand(){
+    let b=document.getElementById("sidebar").classList;
+    if(b.contains("sidebar")){
+        b.replace("sidebar","sidebar-toggled");
     }
-
-    // let c=document.getElementById("content");
-    // try{
-    //     b.attributes.removeNamedItem("style");
-    //     c.attributes.removeNamedItem("style");
-    // }
-    // catch{}
-    // if(window.screen.width>=992){
-    //     if(b.classList.contains("col-lg-1")){
-    //         b.classList.replace("col-lg-1","col-lg-2");
-    //         c.classList.replace("col-lg-11","col-lg-10");
-    //         document.getElementsByClassName("log-name")[0].style.display="inline";
-    //         document.getElementsByClassName("sidebar-item-name")[0].style.display="inline";
-    //     }
-    //     else{
-
-    //         b.classList.replace("col-lg-2","col-lg-1");
-    //         c.classList.replace("col-lg-10","col-lg-11");
-    //         document.getElementsByClassName("log-name")[0].style.display="none";
-    //         document.getElementsByClassName("sidebar-item-name")[0].style.display="none";
-    //     }
-    // }
-    //  else if (window.screen.width >= 767) {
-    //     if(b.classList.contains("col-md-1")){
-    //         b.classList.replace("col-md-1","col-md-2");
-    //         c.classList.replace("col-md-11","col-md-10")
-    //     }
-    //     else{
-    //         b.classList.replace("col-md-2","col-md-1");
-    //         c.classList.replace("col-md-10","col-md-11");
-    //     }
-    // }
-    // else {
-    //     if (b.classList.contains("d-none")) {
-    //         b.style.width = "20%";
-    //         b.classList.remove('d-none');
-    //     }
-    //     else {
-    //         b.classList.add('d-none');
-    //     }
-    // }
+    else{
+        b.replace("sidebar-toggled","sidebar")
+    }
+}
+function tabs(id1,id2){
+    let one=document.getElementById(id1);
+    let second=document.getElementById(id2);
+    console.log(one);
+    console.log(second);
+    if(id1==='product1'){
+        console.log("first")
+        one.style.display="flex";
+        second.style.display="none";
+    }
+    else{
+        console.log("second")
+        one.style.display="flex";
+        second.style.display="none";
+    }
 }
