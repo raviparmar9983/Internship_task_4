d3c04b86faaed6a3579acc257dac776f3e93fa86
raviparmar9983 <parmarravi1162@gmail.com>
2024-02-15 18:25:07 +0530

    changes suggested by the seniour reviewer

--- Asest/js/index.js
+++ Asest/js/index.js
@@ -1,25 +1,37 @@
-function expand(){
-    let b=document.getElementById("sidebar").classList;
-    if(b.contains("sidebar")){
-        b.replace("sidebar","sidebar-toggled");
+function expand() {
+    let a = document.getElementById("sidebar");
+    b = a.classList;
+    if (b.contains("sidebar")) {
+        document.body.classList.replace("sidebar","sidebar-toggled");
+
+        b.replace("sidebar", "sidebar-toggled");
     }
-    else{
-        b.replace("sidebar-toggled","sidebar")
+    else {
+        
+        document.body.classList.replace("sidebar-toggled","sidebar");
+        document.body.classList.remove("sidebar-toggled");
+        b.replace("sidebar-toggled", "sidebar")
     }
 }
-function tabs(id1,id2){
-    let one=document.getElementById(id1);
-    let second=document.getElementById(id2);
-    console.log(one);
-    console.log(second);
-    if(id1==='product1'){
-        console.log("first")
-        one.style.display="flex";
-        second.style.display="none";
+function tabs(id1, id2) {
+    let one = document.getElementById(id1);
+    let second = document.getElementById(id2);
+    let c = document.getElementById("main-product-btn1").style;
+    let d = document.getElementById("main-product-btn2").style;
+    if (id1 === 'product1') {
+        c.backgroundColor = "#5D5FEF"
+        c.color = "#FFFFFF";
+        d.backgroundColor = "transparent";
+        d.color = "black";
+        one.style.display = "flex";
+        second.style.display = "none";
     }
-    else{
-        console.log("second")
-        one.style.display="flex";
-        second.style.display="none";
+    else {
+        d.backgroundColor = "#5D5FEF"
+        d.color = "#FFFFFF";
+        c.backgroundColor = "transparent";
+        c.color = "black";
+        one.style.display = "flex";
+        second.style.display = "none";
     }
 }
